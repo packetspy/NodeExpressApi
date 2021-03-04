1305e8d8da61bfb9fdfac01ad69c20488bebd031
Anderson Tomazeto <packetspy@gmail.com>
2021-03-03 21:15:35 -0300

    Estudos com Backend em Node
    Api simples, recuperando dados de um json

---
+++ config/express.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('config');
+const consign = require('consign');
+
+module.exports = () => {
+    const app = express();
+
+    //Setando variável da aplicação
+    app.set('port', process.env.PORT || config.get('server.port'));
+
+    app.use(bodyParser.json());
+
+    consign({cwd: 'api'})
+    .then('data')
+    .then('controllers')
+    .then('routes')
+    .into(app);
+
+    return app;
+};
