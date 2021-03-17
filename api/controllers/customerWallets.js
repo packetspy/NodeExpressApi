1dd7e40d87c5b28438c7a28eb5bbd1042f0ccf42
Anderson Tomazeto <packetspy@gmail.com>
2021-03-16 22:29:04 -0300

    Adicionando métodos de POST, PUT e DELETE

--- api/controllers/customerWallets.js
+++ api/controllers/customerWallets.js
@@ -1,8 +1,85 @@
+const uuidv4 = require('uuid/v4');
+
 module.exports = app => {
     const customerWalletsDB = app.data.customerWallets;
     const controller = {};
+
+    const {
+      customerWallets: customerWalletsMock
+    } = customerWalletsDB
   
-    controller.listCustomerWallets = (req, res) => res.status(200).json(customerWalletsDB);
-  
-    return controller;
+    controller.listCustomerWallets = (req, res) => res.status(200).json(customerWalletsMock);
+
+    controller.saveCustomerWallets = (req, res) => {
+      customerWalletsMock.data.push({
+        id:uuidv4(),
+        parentId:uuidv4(),
+        name: req.body.name,
+        birthDate: req.body.birthDate,
+        cellphone: req.body.cellphone,
+        phone: req.body.phone,
+        email: req.body.email,
+        occupation: req.body.occupation,
+        state: req.body.state,
+        createdAt: new Date(),
+        updatedAt: null
+      })
+      res.status(201).json(customerWalletsMock);
+    }
+
+    controller.deleteCustomerWallet = (req, res) => {
+      const { customerId } = req.params;
+
+      const foundCustomerIdIndex = customerWalletsMock.data.findIndex(customer => customer.id === customerId);
+
+      if (foundCustomerIdIndex === -1) {
+        res.status(404).json({
+            "message":`Customer "${customerId}" not found.`,
+            "success":false,
+          })
+      } else {
+        customerWalletsMock.data.splice(foundCustomerIdIndex, 1)
+        res.status(200).json({
+          "message":`Customer "${customerId}" removed.`,
+          "success":true,
+        })
+      }
+    }
+
+    controller.updateCustomerWallet = (req, res) => {
+      const { customerId } = req.params;
+
+      const foundCustomerIdIndex = customerWalletsMock.data.findIndex(customer => customer.id === customerId);
+
+      if (foundCustomerIdIndex === -1) {
+        res.status(404).json({
+            "message":`Customer "${customerId}" not found.`,
+            "success":false,
+          })
+      } else {
+
+        const newCustomer = {
+          id: customerId,
+          parentId: req.body.parentId,
+          name: req.body.name,
+          birthDate: req.body.birthDate,
+          cellphone: req.body.cellphone,
+          phone: req.body.phone,
+          email: req.body.email,
+          occupation: req.body.occupation,
+          state: req.body.state,
+          createdAt: null,
+          updatedAt: new Date()
+        } 
+
+        customerWalletsMock.data.splice(foundCustomerIdIndex, 1, newCustomer)
+
+        res.status(200).json({
+          "message": `Customer "${customerId}" updated.`,
+          "success": true,
+          "customerWallet": newCustomer
+        })
+      }
+    }
+      return controller;
   }
--- api/routes/customerWallets.js
+++ api/routes/customerWallets.js
@@ -2,5 +2,10 @@ module.exports = app => {
     const controller = app.controllers.customerWallets
   
     app.route('/api/v1/customer-wallets')
-      .get(controller.listCustomerWallets);
+      .get(controller.listCustomerWallets)
+      .post(controller.saveCustomerWallets);
+
+    app.route('/api/v1/customer-wallet/:customerId')
+      .delete(controller.deleteCustomerWallet)
+      .put(controller.updateCustomerWallet)
   }
